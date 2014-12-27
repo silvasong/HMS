0590618709f5a9e47907b9f84d310431dce5d410
silvasong <silvasong@aliyun.com>
2014-12-27 17:34:43 +0800

    complete infrastructure.

---
+++ src/main/java/com/hms/action/BaseController.java
@@ -0,0 +1,11 @@
+package com.hms.action;
+/**
+ * <p>Title: BaseController.java</p>
+ * <p>Description: Spring MVC Controller基类</p>
+ * @author silvasong Emial:silvasong@aliyun.com
+ * @date 2014年12月27日 下午5:25:00
+ * @version 1.0
+ */
+public class BaseController {
+
+}
